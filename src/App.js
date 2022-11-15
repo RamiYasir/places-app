73b2c37a00e7f5e3fb5bbebb3d44d1536d077a9c
Rami Yasir <ryasir@numarkdigital.co.uk>
2022-11-15 13:50:15 +0000

    add babelrc and jest config

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 function App() {
-  const { version } = window['appConfig'] || 'demo'
+  const version = window['appConfig']?.version || 'Demo'
 
   return (
     <div className="App">
